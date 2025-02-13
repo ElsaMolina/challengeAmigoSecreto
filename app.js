1d6cdc36def39c7b0b398600de76e6dd2ce83b5a
ElsaMolina <62046403+ElsaMolina@users.noreply.github.com>
2025-02-12 18:24:43 -0600

    Agregar función limpiarCaja

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigos = [];
+let Amigos = [];
 
 function capturaDeNombres (){
-    let capturaNombre = document.getElementById("amigo")
+    let capturaNombre = document.getElementById("amigo");
         if (capturaNombre == true){
             console.log (capturaNombre);
         } else {
             alert ("Pls, escribe un nombre");
         }
-    listaAmigos.push(capturaNombre)
+    Amigos.push(capturaNombre);
 }
+
+ function limpiarCaja (){
+    let valorCaja = document.getElementById("amigo");
+    valorCaja.value ="";
+ }

b51e8c83c4a00e8b1a0c47f5df1e7364e891b2b8
ElsaMolina <62046403+ElsaMolina@users.noreply.github.com>
2025-02-12 18:03:56 -0600

    Implementa una función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function capturaDeNombres (){
+    let capturaNombre = document.getElementById("amigo")
+        if (capturaNombre == true){
+            console.log (capturaNombre);
+        } else {
+            alert ("Pls, escribe un nombre");
+        }
+    listaAmigos.push(capturaNombre)
+}

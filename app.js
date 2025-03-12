8bef87b9fa73c38818c2d79556ed87aeba5846be
Elsa M <elsamolina0602@gmail.com>
2025-03-11 20:17:12 -0600

    Terminé el código

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let Amigos = [];
+let intentos =0; 
 
-function capturaDeNombres (){
-    let capturaNombre = document.getElementById("amigo");
-        if (capturaNombre == true){
-            console.log (capturaNombre);
+
+function agregarAmigo(){
+    let capturaNombre = document.getElementById("amigo").value;
+        if (capturaNombre){
+            Amigos.push(capturaNombre);
+            console.log (`Nombre capturado: ${capturaNombre}`);
+            limpiarCaja();
         } else {
             alert ("Pls, escribe un nombre");
-        }
-    Amigos.push(capturaNombre);
-}
-
+        }  
+    }
  function limpiarCaja (){
     let valorCaja = document.getElementById("amigo");
     valorCaja.value ="";
+ }
+
+ function sortearAmigo(){
+    if(Amigos.length === 0){
+        alert("No hay nombres :c");
+        return;
+    }
+
+    let nombreGenerado = Math.floor(Math.random() * Amigos.length);
+    let nombreGanador= Amigos[nombreGenerado];
+    alert (`El nombre sorteado es: ${nombreGanador} `);
+
  }
